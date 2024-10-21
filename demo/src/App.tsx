addbac587e2722a41c61a19d337cb0865ecef8ab
Daeeui Kim <kimeodml@gmail.com>
2024-10-21 22:20:43 +0900

    feat: 테스트 확인

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,45 +1,7 @@
-import { EasyForm } from "react-easyform";
-import "./App.css";
-import "./LoginForm.css";
+import { Input } from 'react-easyform';
 
 function App() {
-  const handleLogin = (formData: { email: string; password: string }) => {
-    console.log("로그인 정보:", formData);
-  };
-
-  return (
-    <EasyForm onSubmit={handleLogin}>
-      {({ email, password, handleEmailChange, handlePasswordChange }) => (
-        <div className="form-container">
-          <div className="form-field">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={handleEmailChange}
-              className="form-input"
-              required
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-              className="form-input"
-              required
-            />
-          </div>
-          <button type="submit" className="form-button">
-            Login
-          </button>
-        </div>
-      )}
-    </EasyForm>
-  );
+  return <Input type="string" id="name" placeholder="테스트 확인" />;
 }
 
 export default App;
